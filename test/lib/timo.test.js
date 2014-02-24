97e5efe36e8f0341b09408c6433782e6123e84da
Drasko Gomboc <drasko.gomboc@gmail.com>
2014-02-24 10:03:22 +0100

    timo: start, stop and duration methods; duration humanize calculations

--- test/lib/timo.test.js
+++ test/lib/timo.test.js
@@ -1,19 +1,53 @@
 
 var should = require("chai").should(),
-    suite = require("../suite"),
-    timo = require(suite.path("timo"));
+    suite  = require("../suite"),
+    timo   = require(suite.path("timo"))
+    moment = require("moment");
 
 describe("timo", function(){
 
     beforeEach(function() {
-
+        timo.timer = {
+            start  : null,
+            end    : null,
+            elapsed: null
+        };
     });
 
-    describe("", function() {
+    describe("start", function() {
 
-        it("", function() {
+        it("shoud set start time", function() {
+            
+            timo.start();
+            timo.timer.start.should.equal(new Date().getTime());
+        });
+    });
+    
+    describe("stop", function() {
 
+        it("shoud set stop time", function() {
+            
+            timo.stop();
+            timo.timer.stop.should.equal(new Date().getTime());
         });
     });
 
+    describe("duration", function() {
+
+        it("shoud measure elapsed time in seconds", function() {
+            
+            timo.timer.start = 1393229088423;
+            timo.timer.stop  = 1393229109470;
+            timo.duration().should.equal("21 seconds 47 milliseconds ");
+            
+            1393232055135
+        });
+        
+        it("shoud measure elapsed time in minutes", function() {
+            
+            timo.timer.start = 1393229088423;
+            timo.timer.stop  = 1393232055135;
+            timo.duration().should.equal("49 minutes 26 seconds ");
+        });
+    });
 });
